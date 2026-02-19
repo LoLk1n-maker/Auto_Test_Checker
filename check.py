2aa485f4b76764734104560268ad5379a520dfcb
LoLkin <lolkin_321@mail.ru>
2026-02-19 04:10:40 +0300

    Добавлена простая проверка теста

--- check.py
+++ check.py
@@ -1,3 +1,49 @@
+<<<<<<< HEAD
+import cv2
+import imutils
+
+def get_cnts(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (3, 3), 0)
+
+    edges = cv2.Canny(gray, 50, 150)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+
+    cnts = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    return cnts
+
+def get_mid(square):
+    summa = square.sum()
+    count = 1
+    for i in square.shape:
+        count *= i
+    return summa / count
+
+def check(image_name: str, sens: int):
+    result = []
+    img = cv2.imread(image_name)
+    cnts = get_cnts(img)
+
+    for cnt in cnts:
+        p = cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, 0.01 * p, True)
+
+        if len(approx) == 4 and 100 < p < 700:
+            answer = img[approx[0, 0, 1]: approx[2, 0, 1], approx[1, 0, 0]: approx[3, 0, 0]]
+            mid = get_mid(answer)
+
+            if mid > sens:
+                cv2.drawContours(img, [approx], -1, (0, 0, 255), 4)
+                result.append(0)
+            else:
+                cv2.drawContours(img, [approx], -1, (0, 255, 0), 4)
+                result.append(1)
+
+        cv2.imwrite(image_name, img)
+=======
 import cv2
 import imutils
 
@@ -43,4 +89,5 @@ def check(image_name: str, sens: int):
                 result.append(1)
 
         cv2.imwrite(image_name, img)
+>>>>>>> 96de381de56a01310ca6c9bf61f24480f87ffddd
     return result[::-1]
--- main.py
+++ main.py
@@ -1,3 +1,100 @@
+<<<<<<< HEAD
+from aiogram import Bot, Dispatcher, executor, types
+from django.contrib.messages.context_processors import messages
+
+from config import token
+from check import *
+
+bot = Bot(token)
+dp = Dispatcher(bot)
+
+start_text = '''Здравствуй, я -  бот предназначенный для автоматической проверки теста'''
+start_markup = types.InlineKeyboardMarkup()
+button1 = types.InlineKeyboardButton('Начать' , callback_data='start')
+start_markup.add(button1)
+
+sensitivity = 100
+
+def get_resultMessage(result):
+    # Проверка в дальнейшем должна включать не конкретный тест, а введеный пользователем, с правильными ответами
+    fin_message = "Вот твои результаты:\n"
+    points = 0
+
+    if result[0:3] == [1, 0 ,0]:
+        fin_message += "1.Верно\n"
+        points +=1
+    else:
+        fin_message += "1.Неверно\n"
+
+    if result[3:7] == [1, 0, 0, 0]:
+        fin_message += "2.Верно\n"
+        points += 1
+    else:
+        fin_message += "2.Неверно\n"
+
+    if result[7:11] == [0, 1, 0 ,0]:
+        fin_message += "3.Верно\n"
+        points += 1
+    else:
+        fin_message += "3.Неверно\n"
+
+    if result[11:] == [0, 1]:
+        fin_message += "4.Верно\n"
+        points += 1
+    else:
+        fin_message += "4.Неверно\n"
+
+    fin_message += f"\nТвои баллы: {points}"
+
+    return fin_message
+
+@dp.message_handler(commands=['start'])
+async def hello(message: types.Message):
+    await bot.send_message(message.chat.id, start_text ,parse_mode='HTML', reply_markup=start_markup)
+
+@dp.message_handler(commands=['parameters'])
+async def prmtrs(message: types.Message):
+    await bot.send_message(message.chat.id, text="Введите параметр порога чувствительности от 0 до 255\n(число, обозначающее средний цвет квадрата, по которому мы судим, есть ли метка в квадрате или нет)\nФормат ввода:\nЧувствительность: 125")
+
+@dp.message_handler(content_types='photo')
+async def resolve(message: types.Message):
+    global sensitivity
+    await message.photo[-1].download(destination_file='img.jpg')
+
+    result = check('img.jpg', sensitivity)
+    resultMessage = get_resultMessage(result)
+    await bot.send_message(message.chat.id, text= resultMessage)
+
+    with open('img.jpg' , 'rb') as photo:
+        await message.answer_photo(photo)
+
+@dp.message_handler()
+async def get_prmts(message: types.Message):
+    global sensitivity
+    text = message.text.lower()
+    num = -1
+
+    try:
+        num = int(message.text[-3:])
+    except:
+        await bot.send_message(message.chat.id, text="Неверный ввод параметра")
+
+    if "чувствительность" in text and not(num >255 or num<0):
+        sensitivity = num
+        await bot.send_message(message.chat.id, text="Успешный ввод параметра")
+    else:
+        await bot.send_message(message.chat.id, text="Неверный ввод")
+
+@dp.callback_query_handler()
+async def callback(callback:types.CallbackQuery):
+    if callback.data == 'start':
+     await callback.bot.send_message(callback.message.chat.id,
+                                '<em>Отправь мне фото решенного теста, в котором отчетливо видно текст</em>',
+                                     parse_mode='HTML')
+     await callback.answer('жду фоток')
+
+executor.start_polling(dp)
+=======
 from aiogram import Bot, Dispatcher, executor, types
 from config import token
 from check import *
@@ -61,3 +158,4 @@ async def callback(callback:types.CallbackQuery):
      await callback.answer('жду фоток')
 
 executor.start_polling(dp)
+>>>>>>> 96de381de56a01310ca6c9bf61f24480f87ffddd
